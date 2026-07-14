2d09659bf5e74718e28209002976961d8e6d2659
Gitzakhar <zmozghovyi@gmail.com>
2026-07-14 12:13:13 -0700

    hiiiii

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from "three";
+import * as THREE from "https://unpkg.com/three@0.179.1/build/three.module.js";
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x87CEEB);
